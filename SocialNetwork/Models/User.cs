72df9323fa3fcbf54d8f084f2f256715425ecbd7
pavyask <pavel.yasko03@gmail.com>
2022-11-11 18:37:04 +0100

    Created User model

---
+++ SocialNetwork/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Models
+{
+    public class User
+    {
+        [Key]
+        public string Login { get; }
+
+        public DateTime CreationDateTime { get; }
+
+        public IEnumerable<User> Friends { get; }
+
+
+        public User(string login)
+        {
+            Login = login;
+            CreationDateTime = DateTime.Now;
+            Friends = new List<User>();
+        }
+    }
+}
