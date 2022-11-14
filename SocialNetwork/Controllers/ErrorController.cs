57cef05bd41ef8e0f51a0d413f577af516c53746
pavyask <pavel.yasko03@gmail.com>
2022-11-14 18:21:29 +0100

    Added custom NotFound Page

---
+++ SocialNetwork/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [ActionName("NotFound")]
+        public IActionResult CustomNotFound()
+        {
+            return View();
+        }
+    }
+}
--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -31,9 +31,12 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseStatusCodePagesWithRedirects("/Error/NotFound");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseStatusCodePagesWithRedirects("/Error/NotFound");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
